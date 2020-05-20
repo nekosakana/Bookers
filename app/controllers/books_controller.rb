2d793a00156a6a0b30483cddb5e71ed079ce0f0f
nekosakana <kiri33pudding@gmail.com>
2020-05-20 21:59:51 +0900

    [Add] show&edit

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -8,6 +8,7 @@ class BooksController < ApplicationController
   end
 
   def show
+    @book = Book.find(params[:id])
   end
 
   def new
@@ -17,10 +18,17 @@ class BooksController < ApplicationController
   def create
     book = Book.new(book_params)
     book.save
-    redirect_to '/books'
+    redirect_to book_path(book.id), notice: 'Book was successfully created.'
   end
 
   def edit
+    @book = Book.find(params[:id])
+  end
+
+  def update
+    book = Book.find(params[:id])
+    book.update(book_params)
+    redirect_to book_path(book), notice: 'Book was successfully created.'
   end
 
   private
